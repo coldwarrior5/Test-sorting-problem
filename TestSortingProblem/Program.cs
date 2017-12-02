eb2ff3084bbc21aefa6a6a32cf3d74593eab4236
Dean Babic <deanbabic@hotmail.com>
2017-12-02 13:42:39 +0100

    Add project files.

---
+++ TestSortingProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestSortingProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
